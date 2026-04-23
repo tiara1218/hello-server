33ece32ba9e3261a9b01b075dae40434aa5b9938
sakura <2259903681@qq.com>
2026-04-23 14:45:06 +0800

    第六周实践

--- src/main/java/com/stu/helloserver/service/UserService.java
+++ src/main/java/com/stu/helloserver/service/UserService.java
@@ -1,10 +1,15 @@
 package com.stu.helloserver.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.stu.helloserver.common.Result;
 import com.stu.helloserver.dto.UserDTO;
+import com.stu.helloserver.entity.User;
 
 public interface UserService {
     Result<String> register(UserDTO userDTO);
     Result<String> login(UserDTO userDTO);
     Result<String> getUserById(Long id);
+
+    // 新增：分页查询用户
+    Result<Page<User>> getUserPage(Integer pageNum, Integer pageSize);
 }
